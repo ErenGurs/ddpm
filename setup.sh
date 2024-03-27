d0c299ff54eaf93c7259f273f0fdab0aa12d763a
ErenGurs <eren.gurses@gmail.com>
2024-03-27 14:28:23 +0000

    Add CIFAR10

--- setup.sh
+++ setup.sh
@@ -4,11 +4,17 @@ export PATH=$PATH:/coreflow/venv/bin/
 alias blobby='aws --endpoint-url https://blob.mr3.simcloud.apple.com --cli-read-timeout 300'
 alias conductor='AWS_EC2_METADATA_DISABLED=true aws --endpoint-url https://conductor.data.apple.com'
 
-echo "Downloading s3://Qixin_Selected_for_FlowAnalysis/Datasets/archive.zip"
-#blobby s3 cp s3://Qixin_Selected_for_FlowAnalysis/Datasets/archive.zip ./
-conductor s3 cp s3://egurses-diffusion/Datasets/archive.zip .
 mkdir -p landscape_img_folder/train
 
-echo
+echo "Downloading s3://egurses-diffusion/Datasets/archive.zip"
+#blobby s3 cp s3://Qixin_Selected_for_FlowAnalysis/Datasets/archive.zip ./
+conductor s3 cp s3://egurses-diffusion/Datasets/archive.zip .
 echo "Uncompressing archive.zip to landscape_img_folder/train/"
 unzip archive.zip -d landscape_img_folder/train/ > /dev/null 2>&1
+
+echo
+mkdir -p cifar10
+echo "Downloading s3://egurses-diffusion/Datasets/CIFAR.zip"
+conductor s3 cp s3://egurses-diffusion/Datasets/CIFAR.zip .
+echo "Uncompressing CIFAR.zip to cifar/"
+unzip CIFAR.zip -d cifar10/ > /dev/null 2>&1
